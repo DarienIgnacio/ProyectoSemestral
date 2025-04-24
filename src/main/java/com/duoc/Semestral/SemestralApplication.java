a18caef9793edf8e589d5cc995500610b8d7ec55
DarienIgnacio <dari.urrutia@duocuc.cl>
2025-04-24 10:54:43 -0400

    Initial

---
+++ src/main/java/com/duoc/Semestral/SemestralApplication.java
@@ -0,0 +1,13 @@
+package com.duoc.Semestral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemestralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemestralApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/duoc/Semestral/SemestralApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duoc.Semestral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemestralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
