2f764910897bd46bf1ac152589ee1497176bef26
Ben-Larrondo29 <ben.moralesl@duocuc.cl>
2025-05-21 19:39:27 -0400

    AlumnoService

--- src/main/java/com/duoc/Semestral/Service/AlumnoService.java
+++ src/main/java/com/duoc/Semestral/Service/AlumnoService.java
@@ -3,8 +3,19 @@ package com.duoc.Semestral.Service;
 import com.duoc.Semestral.Model.Alumno;
 import com.duoc.Semestral.Repository.AlumnoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 public class AlumnoService {
+    @Autowired
+    AlumnoRepository AlumnoRepository;
+
+    public List<Alumno> getAllAlumnos(){return AlumnoRepository.getAllAlumnos();}
+
+    public Alumno getAlumnoById(int id){return AlumnoRepository.getAlumnoById(id);}
+
+    public String addAlumno(Alumno alumno){return AlumnoRepository.addAlumno(alumno);}
+
+    public String deleteAlumno(int id){return AlumnoRepository.deleteAlumno(id);}
+
+    public String updateAlumno(Alumno a){return AlumnoRepository.updateAlumno(a);}
 }
